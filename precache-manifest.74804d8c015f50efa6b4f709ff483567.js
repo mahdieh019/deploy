328a84d827189273d6eaed6b5531a09e6d024a3f
mahdieh019 <mahsanasr2016@gmail.com>
2019-11-04 12:57:17 +0100

    Updates

---
+++ precache-manifest.74804d8c015f50efa6b4f709ff483567.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c876d17f09080c1e95707eb54be899e",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "ad8083cb6db1cdb70719",
+    "url": "/deploy/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "23bc3f0b7a93ee96d663",
+    "url": "/deploy/static/js/2.e5131fc9.chunk.js"
+  },
+  {
+    "revision": "ad8083cb6db1cdb70719",
+    "url": "/deploy/static/js/main.82fc87f4.chunk.js"
+  },
+  {
+    "revision": "cc6396115460cd30cffd",
+    "url": "/deploy/static/js/runtime-main.d84d5cb4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/deploy/static/media/logo.25bf045c.svg"
+  }
+]);
